838f7d51089acfbdf59ff69a7bb923121a1dbd2a
Mubangizi Allan <mubangizia22@gmail.com>
2025-09-04 17:37:21 +0300

    update swagger documentation route

--- cmd/api/app.go
+++ cmd/api/app.go
@@ -96,7 +96,7 @@ func StartServer(port string) {
 	app.Static("/", "./public")
 
 	// Swagger documentation
-	app.Get("/api/docs/*", fiberSwagger.WrapHandler)
+	app.Get("/docs/*", fiberSwagger.WrapHandler)
 
 	// Setup all API routes
 	SetupRoutes(app, natsClient, mongoConfig)
